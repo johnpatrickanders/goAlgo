16232c2295a342bd1ed881ad04efa46fea3933b6
johnpatrickanders <johnpatrickanders@gmail.com>
2020-11-03 14:05:46 -0500

    initialize bars and container

--- client/src/SortingViz/SortingViz.js
+++ client/src/SortingViz/SortingViz.js
@@ -4,25 +4,39 @@ import './SortingViz.css';
 export default function SortingViz() {
   const [array, setArray] = useState([]);
 
+  const maxBarHeight = Math.floor(window.screen.availHeight / 10);
+  const maxBarContainerWidth = Math.floor(window.screen.availWidth / 12);
+  console.log(maxBarHeight)
+
   const resetArray = () => {
     const array = [];
-    for (let i = 0; i < 100; i += 1) {
-      array.push(randomInt(10, 1000))
+    for (let i = 0; i < maxBarContainerWidth; i += 1) {
+      array.push(randomInt(5, maxBarHeight))
     }
+    setArray(array)
   }
 
   useEffect(() => {
     resetArray();
   }, []);
 
+  const mergeSort = () => {
+    const jsSortedArray = array.slice()
+    jsSortedArray.sort((a, b) => a - b)
+  }
+
   return (
-    <>
-      {array.map((value, i) => {
-        <div className="array-bar" key={i}>
-          {value}
+    <div className="array-container">
+      {array.map((value, i) => (
+        <div
+          className="array-bar"
+          key={i}
+          style={{ height: `${value}px` }}
+        >
         </div>
-      })}
-    </>
+      ))}
+      <button onClick={resetArray}>Get New Array</button>
+    </div>
   )
 
 }
@@ -30,3 +44,20 @@ export default function SortingViz() {
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) * min)
 }
+
+function arraysEqual(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i += 1) {
+    if (array1[i] !== array2[i]) {
+      console.log(array1[i], array2[i]);
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log("Should be TRUE:", arraysEqual([1, 5, 4], [1, 5, 4]))
+console.log("Should be FALSE:", arraysEqual([1, 4], [1, 5, 4]))
+console.log("Should be FALSE:", arraysEqual([1, 0, 4], [1, 5, 4]))
+console.log("Should be FALSE:", arraysEqual([1, 0, 4], [1, 50, 4]))
+console.log("Should be FALSE:", arraysEqual([1, 0, 4], [1, 0, -1]))
