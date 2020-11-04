050fb1bc48a428a2b762fdf18faf720e284e902d
johnpatrickanders <johnpatrickanders@gmail.com>
2020-11-04 13:51:35 -0500

    animate bubble sort, but not correctly

--- client/src/SortingViz/SortingViz.js
+++ client/src/SortingViz/SortingViz.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import bubbleSort from './bubbleSort';
 import './SortingViz.css';
 
 export default function SortingViz() {
   const [array, setArray] = useState([]);
 
+  const SPEED = 10;
+
   const maxBarHeight = Math.floor(window.screen.availHeight / 10);
-  const maxBarContainerWidth = Math.floor(window.screen.availWidth / 12);
-  console.log(maxBarHeight)
+  const maxBarContainerWidth = Math.floor(window.screen.availWidth / 120);
 
   const resetArray = () => {
     const array = [];
@@ -20,10 +22,52 @@ export default function SortingViz() {
     resetArray();
   }, []);
 
-  const mergeSort = () => {
-    const jsSortedArray = array.slice()
-    jsSortedArray.sort((a, b) => a - b)
+  const bubbleSortAnimate = () => {
+    const animations = bubbleSort(array);
+    const framedAnimations = [];
+    for (const animation of animations) {
+      framedAnimations.push(animation.comparison);
+      framedAnimations.push(animation.comparison);
+      framedAnimations.push(animation.swap);
+    }
+    console.log(animations)
+    console.log(framedAnimations)
+
+    for (let i = 0; i < framedAnimations.length; i++) {
+      const bars = document.getElementsByClassName('array-bar');
+      const [barOneIdx, barTwoIdx] = framedAnimations[i];
+      const barOne = bars[barOneIdx].style;
+      const barTwo = bars[barTwoIdx].style;
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) { // if the index falls just BEFORE the swap (i.e. on 2nd comparison)
+        setTimeout(() => {
+          const currentBarColor = i % 3 === 0 ? 'blue' : 'green'; // if the index falls on the "swap" value...
+          barOne.backgroundColor = currentBarColor;
+          barTwo.backgroundColor = currentBarColor;
+          // setTimeout(() => {
+          //   bars[comparison[1]].style.backgroundColor = 'red';
+          //   bars[comparison[0]].style.backgroundColor = 'red';
+          // }, (i + 1) * SPEED)
+        }, i * SPEED)
+      } else {
+        setTimeout(() => {
+          // if (barOneIdx < barTwoIdx) {
+
+          // const firstHeight = framedAnimations[barOneIdx];
+          // const secondHeight = framedAnimations[barTwoIdx];
+          const tempHeight = barOne.height;
+          barOne.height = barTwo.height;
+          barTwo.height = tempHeight;
+          // }
+        }, i * SPEED)
+      }
+    }
   }
+
+  // const mergeSort = () => {
+  //   const jsSortedArray = array.slice()
+  //   jsSortedArray.sort((a, b) => a - b)
+  // }
 
   return (
     <div className="array-container">
@@ -36,6 +80,7 @@ export default function SortingViz() {
         </div>
       ))}
       <button onClick={resetArray}>Get New Array</button>
+      <button onClick={bubbleSortAnimate}>Bubble Sort</button>
     </div>
   )
 
@@ -56,8 +101,8 @@ function arraysEqual(array1, array2) {
   return true;
 }
 
-console.log("Should be TRUE:", arraysEqual([1, 5, 4], [1, 5, 4]))
-console.log("Should be FALSE:", arraysEqual([1, 4], [1, 5, 4]))
-console.log("Should be FALSE:", arraysEqual([1, 0, 4], [1, 5, 4]))
-console.log("Should be FALSE:", arraysEqual([1, 0, 4], [1, 50, 4]))
-console.log("Should be FALSE:", arraysEqual([1, 0, 4], [1, 0, -1]))
+// console.log("Should be TRUE:", arraysEqual([1, 5, 4], [1, 5, 4]))
+// console.log("Should be FALSE:", arraysEqual([1, 4], [1, 5, 4]))
+// console.log("Should be FALSE:", arraysEqual([1, 0, 4], [1, 5, 4]))
+// console.log("Should be FALSE:", arraysEqual([1, 0, 4], [1, 50, 4]))
+// console.log("Should be FALSE:", arraysEqual([1, 0, 4], [1, 0, -1]))
--- client/src/SortingViz/bubbleSort.js
+++ client/src/SortingViz/bubbleSort.js
@@ -1,19 +1,24 @@
-
-
-function bubbleSort(array) {
+export default function bubbleSort(array) {
   // Write your code here.
+  const animations = []
   let swapped = true;
   while (swapped) {
     swapped = false;
     for (let i = 0; i < array.length - 1; i++) {
+      const animation = {};
+      animation.comparison = [i, i + 1];
       if (array[i] > array[i + 1]) {
+        animation.swap = [i + 1, i];
         const first = array[i];
         const second = array[i + 1];
         array[i] = second;
         array[i + 1] = first;
         swapped = true;
+      } else {
+        animation.swap = [i, i + 1];
       }
+      animations.push(animation)
     }
   }
-  return array
+  return animations
 }
