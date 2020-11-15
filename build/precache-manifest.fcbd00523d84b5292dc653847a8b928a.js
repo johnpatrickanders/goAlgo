fea866ef27ff5bb4f4a0b5cbb4ebbda6c1074f40
johnpatrickanders <johnpatrickanders@gmail.com>
2020-11-15 18:52:25 -0500

    change "build" to "client"

---
+++ build/precache-manifest.fcbd00523d84b5292dc653847a8b928a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df11bb8249d9881c757b9ebff4fa9c46",
+    "url": "/goAlgo/index.html"
+  },
+  {
+    "revision": "deccf4863eed8c4405ec",
+    "url": "/goAlgo/static/css/main.7b200e7e.chunk.css"
+  },
+  {
+    "revision": "5f371c95545d0bf73615",
+    "url": "/goAlgo/static/js/2.5042aeae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goAlgo/static/js/2.5042aeae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deccf4863eed8c4405ec",
+    "url": "/goAlgo/static/js/main.56b649df.chunk.js"
+  },
+  {
+    "revision": "7c0fef3e0d9808a7fa2c",
+    "url": "/goAlgo/static/js/runtime-main.443ae26b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.289ad8c625c0b37c31ff699e2b416b48.js"
+  "/goAlgo/precache-manifest.fcbd00523d84b5292dc653847a8b928a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goAlgo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
