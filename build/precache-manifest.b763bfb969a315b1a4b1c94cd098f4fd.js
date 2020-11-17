cce518d226cc94345440ba95d1bbe7b055591c87
johnpatrickanders <johnpatrickanders@gmail.com>
2020-11-17 09:34:32 -0500

    new build

---
+++ build/precache-manifest.b763bfb969a315b1a4b1c94cd098f4fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8b589fbada9cb9ffe96d976690a1e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70911338b2b0f80a4d4d",
+    "url": "/static/css/main.7b200e7e.chunk.css"
+  },
+  {
+    "revision": "8b7f4d88d7214f2405f5",
+    "url": "/static/js/2.dfad5138.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dfad5138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70911338b2b0f80a4d4d",
+    "url": "/static/js/main.2199fa82.chunk.js"
+  },
+  {
+    "revision": "48f6bdb2d8f118f185b8",
+    "url": "/static/js/runtime-main.086302fa.js"
+  }
+]);
