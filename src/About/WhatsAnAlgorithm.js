683291f4496e6ada251a03b0ce3652d5bc3fd0f6
johnpatrickanders <johnpatrickanders@gmail.com>
2020-11-18 17:35:33 -0500

    add description of algorithms

--- src/About/WhatsAnAlgorithm.js
+++ src/About/WhatsAnAlgorithm.js
@@ -26,27 +26,33 @@ export default function () {
         domLine.hidden = false;
         if (i === domLines.length - 1) {
           setTimeout(() => {
-            lines = [];
             const exploreToggles = document.getElementById('explore');
             exploreToggles.style.display = 'flex';
             exploreToggles.style.justifyContent = 'center';
             exploreToggles.hidden = false;
           }, 3000)
         }
-      }, 3900 * i)
+      }, 3300 * i)
     }
-  }
-  const explore = () => {
-
   }
   return (
     <div className='lines'>
       {lines.map((line) => {
         return <div className='invisible-lines' hidden> <p>{line}</p> </div>
       })}
-      <div id='explore' hidden='true'>
-        <button onClick={explore}>Explore</button>
-        <a href='/sorting'>...or get started!</a>
+      <div id='explore' hidden={true}>
+        <button >
+          <img onClick={() => window.location = '/sorting'} src='apple-touch-icon.png'></img>
+        </button>
+        <span>
+          <p>An algorithm is a process determined by rules a computer can follow.
+              The process happens for a provided group of things, or data.</p>
+          <p>Perhaps your phone wants to get you to the nearest grocery store, or show you your mutual friend connections.</p>
+          <p>Your phone doesn't just know. Someone taught it how to know. Someone left it a letter.</p>
+          <p>That someone is a Software Engineer, and that letter is an algorithm.*</p>
+          <p style={{ fontSize: '10px' }}>*Yes, it's a bit more complicated than that, but haven't we already argued
+                                            enough this year?</p>
+        </span>
       </div>
     </div>
   )
