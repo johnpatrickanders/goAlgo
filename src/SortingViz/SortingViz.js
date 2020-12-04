6db58273e2a5476eb6b8f0fe95b197958d314fea
johnpatrickanders <johnpatrickanders@gmail.com>
2020-12-04 15:30:48 -0500

    resolve useEffect missing dependency issue

--- src/SortingViz/SortingViz.js
+++ src/SortingViz/SortingViz.js
@@ -52,6 +52,7 @@ export default function SortingViz() {
 
   useEffect(() => {
     resetArray();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const bubbleSortAnimate = () => {
